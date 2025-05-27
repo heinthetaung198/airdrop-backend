d64e2d67472dbf053ec40e2853048bf986d2db34
heinthetaung198 <91749097+heinthetaung198@users.noreply.github.com>
2025-05-27 17:39:03 +0700

    Delete convert-key.js

--- convert-key.js
+++
@@ -1,6 +0,0 @@
-const bs58 = require('bs58'); // ✅ correct way for CommonJS
-
-const secretKeyBase58 = 'N3AUTWN7GYLL5tN7PAVScWLQDfkUtjbjYkA24ac8en1XpcCXRHEdyah2VuVDS4rTYSHsRNQv7ADkg4DPM7m4vr9'; // Phantom export key
-const decoded = bs58.default.decode(secretKeyBase58); // ✅ Fix here
-
-console.log(JSON.stringify([...decoded]));
